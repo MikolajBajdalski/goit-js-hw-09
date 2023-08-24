9747d36d3127fcaa4730d7e3cedbffec67d9dd14
MikolajBajdalski <48803808+MikolajBajdalski@users.noreply.github.com>
2023-08-24 20:23:27 +0200

    taks 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
+import Notiflix from "notiflix";
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({
+          position,
+          delay
+        });
+      } else {
+        reject({
+          position,
+          delay
+        });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let delay = parseInt(form.elements.delay.value);
+  let step = parseInt(form.elements.step.value);
+  let amount = parseInt(form.elements.amount.value);
+
+  let position = 1;
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(position, delay)
+      .then(({
+        position,
+        delay
+      }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({
+        position,
+        delay
+      }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    position++;
+    delay += step;
+  }
+});
